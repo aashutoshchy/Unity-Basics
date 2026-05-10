543fe73e51f1b870c86cebd0b00918d3aabbd523
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-05-10 23:58:54 +0545

    UI

---
+++ 03_Unity_UI_Coco_code/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseUI;
+
+    public void OnRestartPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnGameResumePress()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    public void OnGameExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnEnterPausePress() {
+        pauseUI.SetActive(true);
+    }
+}
